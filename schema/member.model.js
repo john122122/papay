fa8118a03cb105f6ee9389383adc978479ac5520
abbas usman <johnmccain2244@gmail.com>
2023-10-14 14:10:59 +0900

    fix: modify member schema model part 2

--- schema/member.model.js
+++ schema/member.model.js
@@ -34,13 +34,13 @@ const memberSchema = new  mongoose.Schema({
             message: "{VALUE} is not among permitted values"
         }
     },
-    mb_full_name: {
-        type: String,
-        required: false,
-    },
     mb_address: {
         type: String,
         required: false,
+    },
+    mb_description: {
+        type: String,
+        required: false
     },
     mb_image: {
         type: String,
@@ -50,7 +50,38 @@ const memberSchema = new  mongoose.Schema({
         type: Number,
         required: false,
         default: 0
-    }
-});
+    },
+    mb_top: {
+        type: String,
+        required: false,
+        default: 'N',
+        enum: {
+            values: ordernary_enums,
+            messages: "{VALUE} is not among permitted values"
+        },
+    },
+
+    mb_views: {
+        type: Number,
+        required: false,
+        default: 0
+    },
+    mb_likes: {
+        type: Number,
+        required: false,
+        default: 0
+    },
+    mb_follow_cnt: {
+        type: Number,
+        required: false,
+        default: 0
+    },
+    mb_subscriber_cnt: {
+        type: Number,
+        required: false,
+        default: 0
+    },
+    
+}, {timestamps: true});
 
 module.exports = mongoose.model("Member", memberSchema);
