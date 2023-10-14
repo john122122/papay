6b833ff480ec02b82ca8ed7ba800e268abd3b33a
abbas usman <johnmccain2244@gmail.com>
2023-10-14 12:13:37 +0900

    fix: build mongoose connection and member schema model

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ const express = require("express");
 const app = express();
 const router = require("./router");
 
-// MongDB chaqirish
-const db = require("./server").db();
-const mongodb = require("mongodb");
-
 // 1 Kirish codlari
 app.use(express.static("public"));
 app.use(express.json());
