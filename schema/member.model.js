f6be0f0f94fda7ef60cb43f54530bb7a3ca1d3bb
abbas usman <johnmccain2244@gmail.com>
2023-10-25 11:18:53 +0900

    feat: fixed all

--- schema/member.model.js
+++ schema/member.model.js
@@ -26,7 +26,7 @@ const memberSchema = new mongoose.Schema({                     // (class deb nom
     mb_type: {
         type: String,
         required: false,
-        default: "USER",
+        default: "RESTAURANT",
         enum: {
             values: member_type_enums,
             message: "{VALUE} is not among permitted values"     // valueni ichida bulmagan tashqaridan malumot kelsa xatolik bulsin.
