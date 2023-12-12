497e9166d55c40891fd207e888dfc44d7ac3e743
abbas usman <johnmccain2244@gmail.com>
2023-12-12 13:50:09 +0900

    fix: modify getMemberArticles bugs  api

--- controllers/communityController.js
+++ controllers/communityController.js
@@ -5,30 +5,32 @@ let communityController = module.exports;
 
 communityController.imageInsertion = async (req, res) => {
     try {
-        console.log("POST: cont/imageInsertion");
-        assert.ok(req.file, Definer.general_err3);
-        const image_url = req.file.path; 
-
-        res.json({ state: "success", data: image_url });
+      console.log("POST: cont/imageInsertion");
+      assert.ok(req.file, Definer.general_err3);
+      const image_url = req.file.path;
+  
+      res.json({ state: "success", data: image_url });
     } catch (err) {
-        console.log(`ERROR, cont/imageInsertion, ${err.message}`);
-        res.json({ state: "fail", message: err.message });
+      console.log(`ERROR, cont/imageInsertion, ${err.message} `);
+      res.json({ state: "fail", message: err.message });
     }
-};
-
-communityController.createArticle = async (req, res) => {
+  };
+  
+  communityController.createArticle = async (req, res) => {
     try {
-        console.log("POST: cont/createArticle");
-
-        const community = new Community();
-        const result = await community.createArticleData(req.member, req.body);
-        assert.ok(result, Definer.general_err1);
-
-        res.json({ state: "success", data: result });
+      console.log("POST: cont/createArticle");
+  
+      // community service model
+      const community = new Community();
+      const result = await community.createArticleData(req.member, req.body);
+      assert.ok(result, Definer.general_err1);
+  
+      res.json({ state: "success", data: result });
     } catch (err) {
-        console.log(`ERROR, cont/createArticle, ${err.message}`);
-        res.json({ state: "fail", message: err.message });
+      console.log(`ERROR, cont/createArticle, ${err.message} `);
+      res.json({ state: "fail", message: err.message });
     }
+<<<<<<< HEAD
 };
 
 communityController.getMemberArticles = async (req, res) => {
@@ -50,4 +52,29 @@ communityController.getMemberArticles = async (req, res) => {
         res.json({ state: "fail", message: err.message });
     }
 };
+=======
+  };
+  
+  communityController.getMemberArticles = async (req, res) => {
+    try {
+      console.log("GET: cont/getMemberArticles");
+      const community = new Community();
+  
+      const mb_id =
+        req.query.mb_id !== "none" ? req.query.mb_id : req.member?._id;
+      assert.ok(mb_id, Definer.article_err1);
+  
+      const result = await community.getMemberArticlesData(
+        req.member,
+        mb_id,
+        req.query
+      );
+  
+      res.json({ state: "success", data: result });
+    } catch (err) {
+      console.log(`ERROR, cont/getMemberArticles, ${err.message} `);
+      res.json({ state: "fail", message: err.message });
+    }
+  };
+>>>>>>> 1a14425 (fix: modify getMemberArticles bugs  api)
 
--- models/Community.js
+++ models/Community.js
@@ -1,18 +1,28 @@
 const BoArticleModel = require("../schema/bo_article.model");
 const Definer = require("../lib/mistake");
+<<<<<<< HEAD
 const { shapeIntoMongooseObjectId } = require("../lib/config");
 const assert = require("assert");
 const { create } = require("../schema/member.model");
+=======
+const { 
+    shapeIntoMongooseObjectId,
+    board_id_enum_list
+} = require("../lib/config");
+const assert = require("assert");
+
+>>>>>>> 1a14425 (fix: modify getMemberArticles bugs  api)
 
 class Community {
     constructor() {
         this.boArticleModel = BoArticleModel;
     }
 
-    async createArticleData() {
+    async createArticleData(member, data) {
         try {
           data.mb_id = shapeIntoMongooseObjectId(member._id);
           const new_article = await this.saveArticleData(data);
+<<<<<<< HEAD
           return new_article;
         } catch(err) {
           throw err;
@@ -60,6 +70,58 @@ class Community {
           throw err;  
         }
     }
+=======
+          // console.log("new_article:::", new_article);
+          return new_article;
+        } catch (err) {
+          throw err;
+        }
+      }
+    
+      async saveArticleData(data) {
+        try {
+          const article = await this.boArticleModel(data);
+          return await article.save();
+        } catch (mongo_err) {
+          console.log("mongo_err>>>", mongo_err);
+          throw new Error(Definer.auth_err1);
+        }
+      }
+    
+      async getMemberArticlesData(member, mb_id, inquery) {
+        try {
+          const auth_mb_id = shapeIntoMongooseObjectId(member?._id);
+          mb_id = shapeIntoMongooseObjectId(mb_id);
+          const page = inquery["page"] ? inquery["page"] * 1 : 1;
+          const limit = inquery["limit"] ? inquery["limit"] * 1 : 5;
+    
+          const result = await this.boArticleModel
+            .aggregate([
+              { $match: { mb_id: mb_id, art_status: "active" } },
+              { $sort: { createdAt: -1 } },
+              { $skip: (page - 1) * limit },
+              { $limit: limit },
+              {
+                $lookup: {
+                  from: "members",
+                  localField: "mb_id",
+                  foreignField: "_id",
+                  as: "member_data",
+                },
+              },
+              { $unwind: "$member_data" },
+              // todo: check auth liked the chosen target
+            ])
+            .exec();
+          assert.ok(result, Definer.article_err2);
+    
+          return result;
+        } catch (err) {
+          throw err;
+        }
+      }
+    
+>>>>>>> 1a14425 (fix: modify getMemberArticles bugs  api)
 }
 
 module.exports = Community;
