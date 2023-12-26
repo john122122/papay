4184cd6c342399c575aa7f48bc147f287488ec5b
abbas usman <johnmccain2244@gmail.com>
2023-12-26 22:00:25 +0900

    fix: fixsed error

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -17,7 +17,7 @@ memberController.signup = async (req, res) => {
             const token = memberController.createToken(new_member);  //return bulgan valueni tookenga tenglashtiirb olayopmiz.
             res.cookie('access_token', token, {  //cookiesga => acsestokenni va undan hosil bulgan tokenni va
                 maxAge: 6 * 3600 * 1000,         // token bn teng bulgan vaqtni olayopman.
-                httpOnly: true,   // hardoim true bulishi lozim
+                httpOnly: false,   // hardoim true bulishi lozim
             });
 
         res.json({state: 'success', data: new_member}); //standartdagi javob muaffaqiyatli bulsa
@@ -38,7 +38,7 @@ memberController.login = async (req, res) => {
             // console.log("token:::", token);
             res.cookie('access_token', token, {  //cookiesga => acsestokenni va undan hosil bulgan tokenni va
                 maxAge: 6 * 3600 * 1000,         // token bn teng bulgan vaqtni olayopman.
-                httpOnly: true,   // hardoim true bulishi lozim
+                httpOnly: false,   // hardoim true bulishi lozim
             });
 
 
